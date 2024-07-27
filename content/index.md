d86d4b2ddee35aeb7dfa3870a70a7837646e2c4d
Tim <6698857+tims457@users.noreply.github.com>
2024-07-27 15:49:55 -0600

    Quartz sync: Jul 27, 2024, 3:49 PM

--- content/index.md
+++ content/index.md
@@ -8,4 +8,8 @@ Contact me on [Twitter](https://twitter.com/tims457).
 
 ---
 
-[Interesting research papers and blogs.](https://tims457.notion.site/bdb354e530134df4a386a5624badc029?v=20a85de48c2d4bb59a5316acf240c49e&pvs=4)
+[Papers to read](https://tims457.notion.site/bdb354e530134df4a386a5624badc029?v=20a85de48c2d4bb59a5316acf240c49e&pvs=4): A Notion site where I post interesting papers and blog posts I'm reading.
+
+[arXiv Front Page](https://sullivantm.com/arxiv-frontpage): scrapes arXiv daily and uses a language model to list new papers about topics I’m interested in.
+
+[Archive](/Archive) [Notes](/Notes)
--- quartz.config.ts
+++ quartz.config.ts
@@ -28,7 +28,7 @@ const config: QuartzConfig = {
       },
       colors: {
         lightMode: {
-          light: "#faf8f8",
+          light: "#f7f8fa",
           lightgray: "#e5e5e5",
           gray: "#b8b8b8",
           darkgray: "#4e4e4e",
@@ -39,12 +39,12 @@ const config: QuartzConfig = {
           textHighlight: "#fff23688",
         },
         darkMode: {
-          light: "#161618",
+          light: "#161b21",
           lightgray: "#393639",
           gray: "#646464",
           darkgray: "#d4d4d4",
           dark: "#ebebec",
-          secondary: "#7b97aa",
+          secondary: "#4178be",
           tertiary: "#84a59d",
           highlight: "rgba(143, 159, 169, 0.15)",
           textHighlight: "#b3aa0288",
