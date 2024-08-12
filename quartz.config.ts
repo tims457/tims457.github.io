b70b4ffd9ab010e484e9abf9b92cf94a59b1d3d6
Tim Sullivan <6698857+tims457@users.noreply.github.com>
2024-08-11 20:49:54 -0600

    font

--- quartz.config.ts
+++ quartz.config.ts
@@ -24,7 +24,7 @@ const config: QuartzConfig = {
       cdnCaching: true,
       typography: {
         header: "Schibsted Grotesk",
-        body: "Lora",
+        body: "IBM Plex Serif",
         code: "IBM Plex Mono",
       },
       colors: {
